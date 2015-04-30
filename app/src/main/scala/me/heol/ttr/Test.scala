60127ce254f8fa2764e33bceadfb02fd208260a4
Wiktor <wikii122@gmail.com>
2015-04-30 17:47:12 +0200

    Added Hello World with Scala

---
+++ app/src/main/scala/me/heol/ttr/Test.scala
@@ -0,0 +1,11 @@
+package me.heol.ttr
+
+import android.app.Activity
+import android.os.Bundle
+
+class Test extends Activity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_test)
+  }
+}
