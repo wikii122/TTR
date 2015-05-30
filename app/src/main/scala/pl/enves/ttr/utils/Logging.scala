8544d99191d9d2826759f2bb456c7a6e9865793a
Wiktor <wikii122@gmail.com>
2015-05-30 20:02:47 +0200

    Fixed messed up accessors

--- app/src/main/scala/pl/enves/ttr/utils/Logging.scala
+++ app/src/main/scala/pl/enves/ttr/utils/Logging.scala
@@ -6,13 +6,13 @@ import android.util.Log
  * Mixin used to simplify logging
  */
 trait Logging {
-  private[this] val TAG = getClass.getName
+  protected val TAG = getClass.getName
 
-  private def log(msg: String) = Log.d(TAG, msg)
+  protected def log(msg: String) = Log.d(TAG, msg)
 
-  private def warn(msg: String) = Log.w(TAG, msg)
+  protected def warn(msg: String) = Log.w(TAG, msg)
 
-  private def error(msg: String) = Log.e(TAG, msg)
+  protected def error(msg: String) = Log.e(TAG, msg)
 
-  private def wtf(msg: String) = Log.e(TAG, msg)
+  protected def wtf(msg: String) = Log.e(TAG, msg)
 }
