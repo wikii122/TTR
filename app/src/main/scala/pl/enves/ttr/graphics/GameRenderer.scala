282c692c6db257f764755886a4c0a21ef1a23f8f
Wiktor <wikii122@gmail.com>
2015-06-17 20:17:39 +0200

    Changed a bit color theme
    Not sure if it's prettier, but seems to be more enjoyable this way

--- app/src/main/scala/pl/enves/ttr/graphics/GameRenderer.scala
+++ app/src/main/scala/pl/enves/ttr/graphics/GameRenderer.scala
@@ -65,7 +65,7 @@ class GameRenderer(context: Context) extends Renderer with Logging {
 
   override def onSurfaceCreated(gl: GL10, config: EGLConfig) {
     this.synchronized {
-      GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+      GLES20.glClearColor(1.0f, 1.0f, 1.0f, 1.0f)
       GLES20.glClearDepthf(1.0f)
       GLES20.glEnable(GLES20.GL_DEPTH_TEST)
       GLES20.glDepthFunc(GLES20.GL_LEQUAL)
