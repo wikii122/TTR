6b9faeae46c47342468b429ffd2ff27f5c9b1561
Wiktor <wikii122@gmail.com>
2015-06-17 20:37:04 +0200

    Hidden navbar in game
    Honestly I didn't figure out how to hide it again on any action
    once it gets visible, but it's a start, isn't in?

--- app/src/main/scala/pl/enves/ttr/GameActivity.scala
+++ app/src/main/scala/pl/enves/ttr/GameActivity.scala
@@ -2,6 +2,7 @@ package pl.enves.ttr
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import pl.enves.ttr.graphics.GameView
 import pl.enves.ttr.utils.Logging
 
@@ -17,6 +18,11 @@ class GameActivity extends Activity with Logging {
     log("Created")
 
     super.onCreate(state)
+
+    getWindow.getDecorView.setSystemUiVisibility(
+      View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    )
 
     view.startGame()
     setContentView(view)
