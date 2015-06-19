0946c8885d2baced1998447c712146fd6d46404a
Wiktor <wikii122@gmail.com>
2015-06-19 13:38:45 +0200

    Made status bar and navigation bar transparent(-ish)

--- app/src/main/scala/pl/enves/ttr/GameActivity.scala
+++ app/src/main/scala/pl/enves/ttr/GameActivity.scala
@@ -1,8 +1,9 @@
 package pl.enves.ttr
 
-import android.app.Activity
+import android.app.{ActionBar, Activity}
 import android.os.Bundle
-import android.view.View
+import android.support.v7.app.AppCompatActivity
+import android.view.{WindowManager, View}
 import pl.enves.ttr.graphics.GameView
 import pl.enves.ttr.utils.Logging
 
@@ -11,7 +12,7 @@ import pl.enves.ttr.utils.Logging
  *
  * Basically it wraps and configures all things that can display other things.
  */
-class GameActivity extends Activity with Logging {
+class GameActivity extends AppCompatActivity with Logging {
   private[this] lazy val view: GameView = GameView(this)
 
   override def onCreate(state: Bundle): Unit = {
@@ -19,10 +20,7 @@ class GameActivity extends Activity with Logging {
 
     super.onCreate(state)
 
-    getWindow.getDecorView.setSystemUiVisibility(
-      View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-    )
+    setGui()
 
     view.startGame()
     setContentView(view)
@@ -48,5 +46,18 @@ class GameActivity extends Activity with Logging {
     super.onStop()
     // TODO: Remove in production
     this.finish()
+  }
+
+  def setGui() = {
+    getSupportActionBar.hide()
+
+    val window = getWindow
+    window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+    window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+
+    getWindow.getDecorView.setSystemUiVisibility(
+      View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    )
   }
 }
