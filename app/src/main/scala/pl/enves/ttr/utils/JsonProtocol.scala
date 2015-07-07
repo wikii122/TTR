86d8320333d7c7a60a9f4f82ba99f3f8ab323f17
Wiktor <wikii122@gmail.com>
2015-07-07 12:23:36 +0200

    Enhanced JSON protocol

--- app/src/main/scala/pl/enves/ttr/utils/JsonProtocol.scala
+++ app/src/main/scala/pl/enves/ttr/utils/JsonProtocol.scala
@@ -16,4 +16,35 @@ object JsonProtocol extends DefaultJsonProtocol {
       case _ => throw new DeserializationException("Enum string expected")
     }
   }
+
+  implicit object AnyMapFormatter extends JsonFormat[Map[String, Any]] {
+    def write(obj: Map[String, Any]) = JsObject(obj.mapValues(data2json))
+
+    def read(json: JsValue) = json match {
+      case j: JsObject => j.fields.mapValues(json2data)
+      case o => throw new DeserializationException(s"Unparsable type: ${o.getClass} in instance ${o.toString()}")
+    }
+
+
+    def data2json(any: Any): JsValue = any match {
+      case d: Double => JsNumber(d)
+      case n: Int => JsNumber(n)
+      case s: String => JsString(s)
+      case b: Boolean if b => JsTrue
+      case b: Boolean => JsFalse
+      case m: Map[_, _] => write(m.asInstanceOf[Map[String, Any]])
+      case p: Player.Value => p.toJson
+      case o => throw new DeserializationException(s"Unparsable type: ${o.getClass} in instance ${o.toString}")
+    }
+
+    def json2data(js: JsValue): Any = js match {
+      case JsNumber(n) if n.isValidInt => n.intValue()
+      case JsNumber(n) => n.doubleValue()
+      case JsString(s) => s
+      case JsTrue => true
+      case JsFalse => false
+      case j: JsObject => read(j)
+      case o => throw new DeserializationException(s"Unparsable type: ${o.getClass} in instance ${o.toString()}")
+    }
+  }
 }
