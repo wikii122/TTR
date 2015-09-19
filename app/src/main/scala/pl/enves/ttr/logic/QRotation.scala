8243eabead44113cdb0fb6ffc764e10f315f9676
pjan284 <pjan284@gmail.com>
2015-09-19 22:39:05 +0200

    QRotation value extension with add and sub

--- app/src/main/scala/pl/enves/ttr/logic/QRotation.scala
+++ app/src/main/scala/pl/enves/ttr/logic/QRotation.scala
@@ -1,7 +1,13 @@
 package pl.enves.ttr.logic
 
-object QRotation extends Enumeration {
-  val r90, r180, r270 = Value
+object QRotation extends Enumeration(0) {
+  val r0, r90, r180, r270 = Value
+
+  implicit class QRotationValueExtension(rotation: QRotation.Value) {
+    def add(other: QRotation.Value) = QRotation((rotation.id + other.id) % 4)
+
+    def sub(other: QRotation.Value) = QRotation((rotation.id - other.id + 4) % 4)
+  }
 }
 
 object Quadrant extends Enumeration {
