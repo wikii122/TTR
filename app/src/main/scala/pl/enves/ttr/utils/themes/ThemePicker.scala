93bd82e55b2e49fa7a9e9a325a1e0a5df8e9785e
pjan284 <pjan284@gmail.com>
2015-09-22 20:08:26 +0200

    1-pixel border

--- app/src/main/scala/pl/enves/ttr/utils/themes/ThemePicker.scala
+++ app/src/main/scala/pl/enves/ttr/utils/themes/ThemePicker.scala
@@ -39,7 +39,10 @@ class ThemePicker(context: Context, attrs: AttributeSet) extends View(context, a
   //TODO: append user-made themes
 
   private val paints: ArrayBuffer[(Paint, Paint, Paint, Paint)] = themes.map(
-    theme => (makePaint(theme.outer1), makePaint(theme.outer2), makePaint(theme.background), makePaint(Color.BLACK))
+    theme => (makePaint(theme.outer1),
+      makePaint(theme.outer2),
+      makePaint(theme.background),
+      makeBorderPaint(theme.outer1))
   )
 
   private val backgrounds: ArrayBuffer[ColorAndroid] = themes.map(theme => theme.background)
@@ -92,6 +95,15 @@ class ThemePicker(context: Context, attrs: AttributeSet) extends View(context, a
     return p
   }
 
+  private def makeBorderPaint(color: ColorAndroid): Paint = {
+    val p = new Paint(0)
+    p.setStyle(Paint.Style.STROKE)
+    p.setAntiAlias(true)
+    p.setColor(color)
+    p.setStrokeWidth(0)
+    return p
+  }
+
   private def sampleRadius(sideX: Float): Float = {
     val a = (rSide - rCenter) / sideXMax
     val b = rCenter
@@ -138,15 +150,14 @@ class ThemePicker(context: Context, attrs: AttributeSet) extends View(context, a
 
   private def drawSample(canvas: Canvas, x: Float, y: Float, theme: Int): Unit = {
     val radius = sampleRadius(Math.abs(centerX - x))
-
-    canvas.drawCircle(x, y, radius, paints(theme)._4)
-    canvas.drawCircle(x, y, 0.8f * radius, paints(theme)._3)
+    canvas.drawCircle(x, y, radius, paints(theme)._3)
     val size = 0.6f * radius
     val rect: RectF = new RectF(x - size, y - size, x + size, y + size)
     canvas.drawArc(rect, 0, 90, true, paints(theme)._2) //Second
     canvas.drawArc(rect, 90, 180, true, paints(theme)._1) //First
     canvas.drawArc(rect, 180, 270, true, paints(theme)._2) //Third
     canvas.drawArc(rect, 0, -90, true, paints(theme)._1) //Fourth
+    canvas.drawCircle(x, y, radius, paints(theme)._4)
   }
 
   private def readDefaultThemes: mutable.ArrayBuffer[Theme] = {
