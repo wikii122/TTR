4661a3939dc8ef912824fbb4cbaae6a428abeeaa
pjan284 <pjan284@gmail.com>
2016-01-08 14:00:08 +0100

    unused prefs field deleted from ExtendedActivity

--- app/src/main/scala/pl/enves/androidx/ExtendedActivity.scala
+++ app/src/main/scala/pl/enves/androidx/ExtendedActivity.scala
@@ -10,20 +10,12 @@ import scala.reflect.{ClassTag, classTag}
 abstract class ExtendedActivity extends AppCompatActivity with ContextRegistry with Logging {
   type ID = Int
 
-  protected var prefs: Option[SharedPreferences] = None
-
   private lazy val handler = new Handler(Looper.getMainLooper)
   private lazy val uiThread = Looper.getMainLooper.getThread
 
   protected def find[A](id: ID) = findViewById(id).asInstanceOf[A]
 
   protected def intent[A: ClassTag] = new Intent(this, classTag[A].runtimeClass)
-
-  override def onCreate(savedInstanceState: Bundle) {
-    super.onCreate(savedInstanceState)
-
-    prefs = Some(getSharedPreferences("preferences", Context.MODE_PRIVATE))
-  }
 
   protected def UiThread(f: () => Unit) = {
     lazy val runnable = new Runnable() {
