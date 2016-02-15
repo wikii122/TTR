9a4382dcc6348a58b846fedc8776ce5d7e538ea7
pjan284 <pjan284@gmail.com>
2016-02-15 21:31:34 +0100

    main menu entry for invitations

--- app/src/main/scala/pl/enves/ttr/utils/start/MainMenuFragment.scala
+++ app/src/main/scala/pl/enves/ttr/utils/start/MainMenuFragment.scala
@@ -3,7 +3,7 @@ package pl.enves.ttr.utils.start
 import android.graphics.Typeface
 import android.os.Bundle
 import android.view.{LayoutInflater, View, ViewGroup}
-import android.widget.Button
+import android.widget.{Button, RelativeLayout}
 import pl.enves.androidx.Logging
 import pl.enves.androidx.color.ColorUiTweaks
 import pl.enves.androidx.helpers._
@@ -23,15 +23,22 @@ class MainMenuFragment extends StyledFragment with ColorUiTweaks with Logging {
     super.onStart()
 
     val view = getView
+    val invitationsButton = (find[Button](view, R.id.button_invitations), find[Button](view, R.id.button_invitations_prompt))
     val newGameButton = (find[Button](view, R.id.button_new), find[Button](view, R.id.button_new_prompt))
     val continueGameButton = (find[Button](view, R.id.button_continue), find[Button](view, R.id.button_continue_prompt))
     val settingsButton = (find[Button](view, R.id.button_settings), find[Button](view, R.id.button_settings_prompt))
 
     setContinueButtonEnabled(GameState.active)
+    setInvitationsNumber(0)
 
+    invitationsButton onClick onInvitations
     newGameButton onClick onNewGame
     continueGameButton onClick onContinue
     settingsButton onClick onSettings
+  }
+
+  private[this] def onInvitations(v: View): Unit = {
+    //TODO
   }
 
   private[this] def onNewGame(v: View): Unit = {
@@ -48,10 +55,12 @@ class MainMenuFragment extends StyledFragment with ColorUiTweaks with Logging {
 
   override def setTypeface(typeface: Typeface): Unit = {
     val view = getView
+    val invitationsButton = (find[Button](view, R.id.button_invitations), find[Button](view, R.id.button_invitations_prompt))
     val newGameButton = (find[Button](view, R.id.button_new), find[Button](view, R.id.button_new_prompt))
     val continueGameButton = (find[Button](view, R.id.button_continue), find[Button](view, R.id.button_continue_prompt))
     val settingsButton = (find[Button](view, R.id.button_settings), find[Button](view, R.id.button_settings_prompt))
 
+    invitationsButton.setTypeface(typeface)
     newGameButton.setTypeface(typeface)
     continueGameButton.setTypeface(typeface)
     settingsButton.setTypeface(typeface)
@@ -59,10 +68,12 @@ class MainMenuFragment extends StyledFragment with ColorUiTweaks with Logging {
 
   override def setColorTheme(theme: Theme): Unit = {
     val view = getView
+    val invitationsButton = (find[Button](view, R.id.button_invitations), find[Button](view, R.id.button_invitations_prompt))
     val newGameButton = (find[Button](view, R.id.button_new), find[Button](view, R.id.button_new_prompt))
     val continueGameButton = (find[Button](view, R.id.button_continue), find[Button](view, R.id.button_continue_prompt))
     val settingsButton = (find[Button](view, R.id.button_settings), find[Button](view, R.id.button_settings_prompt))
 
+    invitationsButton.setTextColor(theme.color1, theme.color2)
     newGameButton.setTextColor(theme.color1, theme.color2)
     continueGameButton.setTextColor(colorStateList(theme.color1, 0.25f), colorStateList(theme.color2, 0.25f))
     settingsButton.setTextColor(theme.color1, theme.color2)
@@ -77,6 +88,22 @@ class MainMenuFragment extends StyledFragment with ColorUiTweaks with Logging {
         continueGameButton.get.enable()
       } else {
         continueGameButton.get.disable()
+      }
+    }
+  }
+
+  def setInvitationsNumber(number: Int): Unit = {
+    val view = getView
+    if (view != null) {
+      val invitationsLayout = find[RelativeLayout](view, R.id.layout_invitations)
+
+      if (number != 0) {
+        invitationsLayout.setVisibility(View.VISIBLE)
+        val invitationsButton = find[Button](view, R.id.button_invitations)
+        val text = getActivity.getResources.getText(R.string.invitations).toString
+        invitationsButton.setText(text.format(number))
+      } else {
+        invitationsLayout.setVisibility(View.GONE)
       }
     }
   }
