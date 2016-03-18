b51c7cdb763152a110a8486d565ec5dfd8b97c3f
pjan284 <pjan284@gmail.com>
2016-03-18 17:10:09 +0100

    trycatch around asset io

--- app/src/main/scala/pl/enves/ttr/graphics/texture/AssetTexture.scala
+++ app/src/main/scala/pl/enves/ttr/graphics/texture/AssetTexture.scala
@@ -1,25 +1,32 @@
 package pl.enves.ttr.graphics.texture
 
-import java.io.InputStream
+import java.io.{IOException, InputStream}
 
 import android.content.Context
 import android.graphics.BitmapFactory
-import pl.enves.androidx.IOUtils
+import pl.enves.androidx.{IOUtils, Logging}
 import pl.enves.ttr.graphics.TextureProvider
 
-class AssetTexture(context: Context, path: String) extends TextureProvider {
+class AssetTexture(context: Context, path: String) extends TextureProvider with Logging {
 
   def decode(path: String): Int = {
-    val stream: InputStream = context.getAssets.open(path)
-    val rawData = IOUtils.readBytes(stream)
-    stream.close()
+    try {
+      val stream: InputStream = context.getAssets.open(path)
+      val rawData = IOUtils.readBytes(stream)
+      stream.close()
 
-    val bitmap = BitmapFactory.decodeByteArray(rawData, 0, rawData.length)
+      val bitmap = BitmapFactory.decodeByteArray(rawData, 0, rawData.length)
 
-    val texture = createTexture(bitmap)
-    bitmap.recycle()
+      val texture = createTexture(bitmap)
+      bitmap.recycle()
 
-    return texture
+      return texture
+    } catch {
+      case e: IOException =>
+        error(e.getMessage)
+        return 0
+    }
+
   }
 
   override def getTexture: Int = decode(path)
